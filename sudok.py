45cdf8a91b3ae58ca1dcda14647686b345090107
ownotaku <98224904+ownotaku@users.noreply.github.com>
2022-06-27 20:57:08 +0300

    Add files via upload

--- sudok.py
+++ sudok.py
@@ -1,8 +1,5 @@
-import time
 from tkinter import *
-import tkinter.messagebox
 from random import sample
-from collections import Counter
 from PIL import Image, ImageTk
 
 root = Tk()
@@ -12,58 +9,54 @@ root["bg"] = "white"
 root.geometry("1000x700")
 root.resizable(width=False, height=False)
 
-# открывает изображение
+# открываем изображение
 image = ImageTk.PhotoImage(Image.open('sydoku.png'))
 
-# функция, которая создаёт основное меню
+# функция создаёт основное меню
 def menu() -> None:
 
-    # создание основного меню
-
-    # создаёт frame для основного меню
+    # frame для основного меню
     frame = Frame(root, bg="white", width=1000, height=700)
     frame.pack()
 
-    # функция, которая уничтожает главное окно и создаёт окно с правилами игры
+    #  уничтожает меню и создаёт окно с правилами игры
     def how_play() -> None:
         frame.destroy()
         rules()
 
-    # функция, которая уничтожает главное окно и создаёт окно с полем судоку
+    # уничтожает меню и создаёт поле судоку
     def play() -> None:
         frame.destroy()
-        game()
+        choose_difficulty()
 
-    # создаёт кнопки, картинку и надпись
-    Label(frame, bg="white", fg="black", text="Генератор Судоку", font=("Arial", 25)).place(relheight=0.1, relwidth=0.3,
-                                                                                            rely=0.05, relx=0.35)
+    # кнопки, картинка и надпись
+    Label(frame, bg="white", fg="black", text="Генератор Судоку", font=("Arial", 25)).place(relheight=0.1, relwidth=0.4,
+                                                                                            rely=0.05, relx=0.3)
 
     canvas = Canvas(frame, width=400, height=400)
     canvas.create_image(2, 2, anchor=NW, image=image)
     canvas.place(rely=0.18, relx=0.3)
 
-    btn_play = Button(frame, text="Начать играть", bg="grey", fg="black", bd=0, font=("Arial", 20), command=play)
+    btn_play = Button(frame, text="Сгенерировать поле", bg="grey", fg="black", bd=0, font=("Arial", 20), command=play)
     btn_play.place(relwidth=0.3333, relheight=0.1, relx=0.111111, rely=0.8)
 
     btn_rules = Button(frame, text="Правила игры", bg="grey", fg="black", bd=0, font=("Arial", 20), command=how_play)
     btn_rules.place(relwidth=0.3333, relheight=0.1, relx=0.5555, rely=0.8)
 
 
-# создаёт окно с правилами игры
+# окно с правилами
 def rules() -> None:
 
-    #создание окна с правилами
-
-    # создаёт frame для окна с правилами
+    # frame для окна с правилами
     frame = Frame(root, bg="white", width=1000, height=700)
     frame.pack()
 
-    # функция, которая удаляет окно с правилами и создаёт окно с самой игрой
+    # удаляет окно с правилами и и выходит в меню
     def back() -> None:
         frame.destroy()
         menu()
 
-    # создаёт текст с правилом и кнопку "назад"
+    # текст с правилами и выход
     rule = Text(frame, bg="white", bd=0, wrap=WORD, font=("Arial", 20))
     rule.insert(END,
                 r"Игровое поле представляет собой квадрат размером 9x9, разделённый на меньшие квадраты стороной в 3 клетки. Таким образом, всё игровое поле состоит из 81 клетки. В них уже в начале игры стоят некоторые числа (от 1 до 9), называемые подсказками. От игрока требуется заполнить свободные клетки цифрами от 1 до 9 так, чтобы в каждой строке, в каждом столбце и в каждом малом квадрате 3x3 каждая цифра встречалась бы только один раз.")
@@ -73,20 +66,45 @@ def rules() -> None:
     btn_back = Button(frame, text="Назад", bg="grey", fg="black", bd=0, font=("Arial", 20), command=back)
     btn_back.place(relwidth=0.3333, relheight=0.1, relx=0.3333, rely=0.8)
 
+# окно с выбором сложности
+def choose_difficulty() -> None:
 
-# функция создаёт окно с игрой
-def game() -> None:
+    frame = Frame(root, bg="white", width=1000, height=700)
+    frame.pack()
 
-    # запуск окна с игрой
+    # споле легкого уровня
+    def lite():
+        frame.destroy()
+        game(6)
 
-    # сохраняет время начала игры
-    start_time = time.time()
+    # поле среднего уровня
+    def medium():
+        frame.destroy()
+        game(5)
 
-    # создаёт canvas для этого окна
+    # поле сложного уровня
+    def hard():
+        frame.destroy()
+        game(4)
+
+    # кнопки
+    Label(frame, bg="white", bd=0, text="Выберете уровень сложности:", font=("Arial", 25)).place(relheight=0.11, relwidth=0.6,
+                                                                                            rely=0.1, relx=0.25)
+    Button(frame, bg="grey", text="Лёгкий", fg="black", font=("Arial", 20), bd=0, command=lite).place(relwidth=0.3333, relheight=0.1, relx=0.3333, rely=0.3)
+    Button(frame, bg="grey", text="Средний", fg="black", font=("Arial", 20), bd=0, command=medium).place(relwidth=0.3333, relheight=0.1, relx=0.3333, rely=0.5)
+    Button(frame, bg="grey", text="Сложный", fg="black", font=("Arial", 20), bd=0, command=hard).place(relwidth=0.3333, relheight=0.1, relx=0.3333, rely=0.7)
+
+# окно поля
+def game(difficulty) -> None:
+
+    # создаём canvas для этого окна
     canvas = Canvas(root, bg="white", bd=0)
     canvas.place(relheight=1, relwidth=1)
 
-    # создаёт заполненное судоку
+    field = Canvas(canvas, bg='white', bd=0)
+    field.place(relheight=1, width=626+72, x=0, y=0)
+
+    # создает судоку
     base = 3
     side = 9
 
@@ -102,22 +120,26 @@ def game() -> None:
     nums = shuffle(range(1, base * base + 1))
     board = [[nums[pattern(r, c)] for c in cols] for r in rows]
 
-    # удаляет некокорые значения из судоку
+    # создает ответы
+    global all_board
+    all_board = []
+    for x in board:
+        all_board.extend(x if isinstance(x, list) else [x])
+
+    # убирает лишние значения
     squares = side * side
-    empties = squares * 3 // 4
-    for p in sample(range(squares), empties):
+    empties = squares * 3 // difficulty
+    a = sample(range(squares), empties)
+    for p in a:
         board[p // side][p % side] = 0
 
-    # функция, которая проверяет правильность заполнения поля
     def check() -> None:
-        # сохраняет то, что ввёл пользователь
-        rez = {i: globals()[f"entry{i}"].get() for i in range(81)}
 
-        # сортирует значение по строкам
+        # сортировка по строкам
         stroka = [{j + i * 9: rez[j + i * 9] for j in range(9)} for i in range(9)]
-        # сортирует значение по столбикам
+        # сортировка по столбикам
         stolb = [{j * 9 + i: stroka[j][j * 9 + i] for j in range(9)} for i in range(9)]
-        # сортирует значение по квадратам
+        # сортировка по квадратам
         kwad = []
         for q in range(0, 7, 3):
             for w in range(0, 7, 3):
@@ -127,125 +149,41 @@ def game() -> None:
                         spis[(i + q) * 9 + j + w] = stroka[i + q][(i + q) * 9 + j + w]
                 kwad.append(spis)
 
-        # проверяет строку и делает поле с цифрами белыми или красными
-        rezstroka = True
-        for i in stroka:
-            vstroka = [v1 for k1, v1 in i.items()]
-            c = Counter(vstroka)
-            for k2, v2 in c.items():
-                if v2 > 1 and k2 != "":
-                    for k3, v3 in i.items():
-                        if v3 == k2:
-                            globals()[f"entry{k3}"].configure({"background": "red"})
-                    rezstroka = False
-                elif k2 == "":
-                    for k3, v3 in i.items():
-                        if v3 == k2:
-                            globals()[f"entry{k3}"].configure({"background": "yellow"})
-                    rezstroka = False
-                elif v2 == 1:
-                    for k3, v3 in i.items():
-                        if v3 == k2:
-                            globals()[f"entry{k3}"].configure({"background": "white"})
 
-        # проверяет столбики и делает квадраты с неправильными числами красными
-        rezstolb = True
-        for i in stolb:
-            vstolb = [v1 for k1, v1 in i.items()]
-            c = Counter(vstolb)
-            for k2, v2 in c.items():
-                if v2 > 1 and k2 != "":
-                    for k3, v3 in i.items():
-                        if v3 == k2:
-                            globals()[f"entry{k3}"].configure({"background": "red"})
-                    rezstolb = False
+    # функция ответов
+    def answer():
+        for i in range(81):
+            exec(f'v{i}.set(all_board[i])')
+            exec(f'root.after_idle(lambda: entry{i}.config(validate="key"))')
 
-        # проверяет квадраты и делает с неправильными числами красными
-        rezkwad = True
-        for i in kwad:
-            vkwad = [v1 for k1, v1 in i.items()]
-            c = Counter(vkwad)
-            for k2, v2 in c.items():
-                if v2 > 1:
-                    for k3, v3 in i.items():
-                        if v3 == k2 and k2 != "":
-                            globals()[f"entry{k3}"].configure({"background": "red"})
-                    rezkwad = False
+    def validate() -> bool:
+        return False
 
-        # если всё заполнено правильно, то показывает сообщение об этом, уничтожает это окно и создаёт меню
-        if rezstroka and rezstolb and rezkwad:
-            nonlocal canvas
-
-            tkinter.messagebox.showinfo(title="Уровень пройден!",
-                                        message=f"Поздравляю! Вы прошли судоку за {now_minutes} минут и {now_second} секунд!")
-
-            canvas.destroy()
-            menu()
-
-    # функция уничтожает окно и создаёт меню
     def exit() -> None:
         nonlocal canvas
 
         canvas.destroy()
         menu()
 
-    # функция, которая всё время обновляет значения времени
-    def update_time() -> None:
-        nonlocal lbl_time, canvas
-        global now_minutes, now_second
-
-        now_time = int(time.time() - start_time)
-        now_minutes = now_time // 60
-        now_second = now_time % 60
-
-        if len(str(now_minutes)) <= 1: now_minutes = "0" + str(now_minutes)
-        if len(str(now_second)) <= 1: now_second = "0" + str(now_second)
-
-        lbl_time.configure(text=f"Прошло {now_minutes} : {now_second}")
-
-        canvas.after(1000, update_time)
-
-    # функция, которая очищает поле
-    def clear() -> None:
-        for i in range(81):
-            exec(f"entry{i}.configure(background='white')\nentry{i}.delete(0, END)")
-
-    # функция, которая проверяет, что ввели
-    num = "1 2 3 4 5 6 7 8 9".split()
-    def validate(new_value: str, old_value: str, action: str, widget: str) -> None:
-        color = globals()[widget].cget("background")
-        if  color == "red" or color=="yellow":
-            globals()[widget].configure(background="white")
-
-        return new_value == "" or new_value in num and len(old_value) < 1 or action == "0"
-
-    # создаёт надпись и кнопки
-    lbl_time = Label(canvas, bg="white", fg="black", bd=0, font=("Arial", 20))
-    lbl_time.place(relwidth=0.25, relheight=0.1, relx=0.7125, rely=0.1)
-
-    btn_restart = Button(canvas, text="Начать сначала", bg="grey", fg="black", bd=0, font=("Arial", 20),
-                         command=clear)
-    btn_restart.place(relwidth=0.25, relheight=0.1, relx=0.7125, rely=0.3)
+    # кнопки
+    btn_answer = Button(canvas, text="Ответы", bg="grey", fg="black", bd=0, font=("Arial", 20), command=answer)
+    btn_answer.place(relwidth=0.25, relheight=0.1, relx=0.7125, rely=0.2666666666666667)
 
     btn_exit = Button(canvas, text="Выйти", bg="grey", fg="black", bd=0, font=("Arial", 20), command=exit)
-    btn_exit.place(relwidth=0.25, relheight=0.1, relx=0.7125, rely=0.5)
+    btn_exit.place(relwidth=0.25, relheight=0.1, relx=0.7125, rely=0.6333333333333334)
 
-    btn_check = Button(canvas, text="Завершить", bg="grey", fg="black", bd=0, font=("Arial", 20), command=check)
-    btn_check.place(relwidth=0.25, relheight=0.1, relx=0.7125, rely=0.7)
-
-    # вызывает функцию, которая обновляет время
-    update_time()
-
-    # создаёт поле судоку
+    # поле судоку
     y_entry = 25
     x_entry = 25
     number = 0
     for line in board:
         for i in line:
-            exec(
-                f"global entry{number}\nentry{number} = Entry(canvas, validate='key', validatecommand=(root.register(validate), '%S', '%s', '%d', 'entry{number}'), bg='white', fg='black', justify=CENTER, font=('Arial', 20), relief=FLAT)\nentry{number}.place(width=70, height=70, x=x_entry, y=y_entry)")
+            exec(f'global v{number}\nv{number} = StringVar()')
             if i != 0:
-                exec(f"entry{number}.insert(END, {i})")
+                exec(f'v{number}.set(i)')
+            exec(
+                f"global entry{number}\nentry{number} = Entry(field, validate='key', validatecommand=(root.register(validate),), textvariable=v{number}, bg='white', fg='black', justify=CENTER, font=('Arial', 20), relief=FLAT)\nentry{number}.place(width=70, height=70, x=x_entry, y=y_entry)")
+            if i != 0:
                 exec(f"entry{number}['state'] = 'disable'")
 
             x_entry += 72
@@ -253,25 +191,24 @@ def game() -> None:
         x_entry = 25
         y_entry += 72
 
-    # рисует линии по y между клетками поля
+    # линии по y между клетками поля
     x_line = 25
     for i in range(1, 9):
         if i % 3 == 0:
-            canvas.create_line(x_line + 71, 25, x_line + 71, 672, width=2)
+            field.create_line(x_line + 71, 25, x_line + 71, 672, width=2)
         else:
-            canvas.create_line(x_line + 71, 25, x_line + 71, 672, width=1)
+            field.create_line(x_line + 71, 25, x_line + 71, 672, width=1)
         x_line += 72
 
-    # рисует линии по x между клетками поля
+    # линии по x между клетками поля
     y_line = 25
     for i in range(1, 9):
         if i % 3 == 0:
-            canvas.create_line(25, y_line + 71, 672, y_line + 71, width=2)
+            field.create_line(25, y_line + 71, 672, y_line + 71, width=2)
         else:
-            canvas.create_line(25, y_line + 71, 672, y_line + 71, width=1)
+            field.create_line(25, y_line + 71, 672, y_line + 71, width=1)
         y_line += 72
 
-#запускает код
 if __name__ == "__main__":
     menu()
     root.mainloop()
